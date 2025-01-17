17c5d686d071d1bbc48905c63cce2e22d693ca9a
LatifurRatul <latifursoikot@gmail.com>
2025-01-17 19:37:25 +0600

    add seller product, policy section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
     <div className="flex flex-col sm:flex-row border border-gray-400">
       {/* Hero Left Side */}
 
-      <div className="w-full sm:w-1/2 flex items-center justify-content py-10 sm:py-0">
+      <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
         <div className="text-[#414141]">
           <div className="flex items-center gap-2">
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
           </div>
 
-          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
             Latest Arrivals
           </h1>
           <div className="flex items-center gap-2">
@@ -25,11 +25,7 @@ const Hero = () => {
 
       {/* Hero Right Side */}
 
-      <img
-        className="w-full md:w-11 sm:w-1/2"
-        src={assets.hero_img}
-        alt="hero-img"
-      />
+      <img className="w-full sm:w-1/2" src={assets.hero_img} alt="hero-img" />
     </div>
   );
 };
