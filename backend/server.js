1682b7ef913373b697dbd4ea61b7b66b5562cdae
Md-LatifurRatul <latifurratulislam@gmail.com>
2025-02-08 03:08:27 +0600

    add route, controller, models, config, middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 // App configuration
 
 const app = express()
 
 const port = process.env.PORT || 4000
+
+
+connectDB()  //Connect MongoBD
+connectCloudinary()  //Connect Cloudinary Storage
 
 //MiddleWares
 
@@ -15,6 +23,9 @@ app.use(express.json())
 app.use(cors())
 
 //API endpoints
+
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 app.get('/', (req, res) => {
     res.send("API Working")
